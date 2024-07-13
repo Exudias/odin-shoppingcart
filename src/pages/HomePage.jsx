22bed2dc4e272c804d21cd2e732008ff2a094f4d
Exudias <editoo31@gmail.com>
2024-07-13 23:03:32 +0300

    Various adjustments to scrolling text

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import SplashImage from "../components/SplashImage";
 import ScrollingText from "../components/ScrollingText";
 
 function HomePage() {
-    const scrollingText = "Promotions every day! Get your free X today. Alternatively, check out our cool new items.";
+    const scrollingText = "Promotions every day! Get your free X today. Alternatively, check out our cool new items.dsaadsdsaasddsadasadsdasdsadsadsdassa gsfdg sahagsgsa ds fdshfsdahsfadhlhf lasdfa hfasdhlfadhlk fashl kafhshlfadh lfad hfadsh lfasdhl falh ksdlkh fsdl khafsdlhkfads lhka flshkdlkhafdslkhfadslkhfasdlkhfasldkhlhkfadshlkfsdal hkafds lhkafsd hlkfs dhlkf dhslkf ahldks hklafsdhlk";
 
     return <>
         <Helmet>
