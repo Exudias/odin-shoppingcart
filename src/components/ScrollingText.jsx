32b9b765e104c9d0900eeb6ced0e629aa02470a4
Exudias <editoo31@gmail.com>
2024-07-13 19:35:34 +0300

    Add scrolling text to home page

---
+++ src/components/ScrollingText.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import styles from './ScrollingText.module.css';
+
+function ScrollingText({text})
+{
+    return <div className={styles.container}>
+        <div className={styles.scrollingText}>
+            {text}
+        </div>
+    </div>
+}
+
+ScrollingText.propTypes = {
+    text: PropTypes.string,
+}
+
+export default ScrollingText;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,17 @@
 import { Helmet } from "react-helmet";
 import SplashImage from "../components/SplashImage";
+import ScrollingText from "../components/ScrollingText";
 
 function HomePage() {
+    const scrollingText = "Promotions every day! Get your free X today. Alternatively, check out our cool new items.";
+
     return <>
         <Helmet>
             <title>Faux Gear | Home</title>
         </Helmet>
         <div className="page-content">
             <SplashImage src="./home_splash.jpg" alt=""/>
+            <ScrollingText text={scrollingText}/>
         </div>
     </>
 }
