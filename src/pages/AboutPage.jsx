7ea83fac2a06a7ae918af0fcb39a4f358f26ec82
Exudias <editoo31@gmail.com>
2024-07-15 09:49:54 +0300

    Create about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,12 +1,24 @@
 import { Helmet } from "react-helmet";
+import styles from "./AboutPage.module.css";
 
 function AboutPage() {
     return <>
         <Helmet>
             <title>Faux Gear | About</title>
         </Helmet>
-        <div className="page-content">
-            <h1>About page</h1>
+        <div className={`page-content ${styles.main}`}>
+            <h1>This is not a real store!</h1>
+            <h2>Created using the FakeStore API for products and ReactJS for the front end.</h2>
+            <div className={styles.contacts}>
+                <a href="https://github.com/Exudias/odin-shoppingcart" target="_blank">
+                    <img className={styles.icon} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" />
+                </a>
+                <a href="https://www.linkedin.com/in/e-sarkisyan/" target="_blank">
+                    <img className={styles.icon} src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linkedin/linkedin-plain.svg" />
+                </a>
+            </div>
+            
+            <p>&copy; Exudias 2024. All rights reserved.</p>
         </div>
     </>
 }
